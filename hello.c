68126d0c972b1b75a7176e0af8138492d38fd8e7
oliviermizero <59984459+oliviermizero@users.noreply.github.com>
2020-01-21 20:40:38 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,12 @@
 /* "Hello World", My first program
 Name: Martial Sandeu
+Collaborator: Olivier Niyonshuti Mizero
 Date: 2020/01/21
 */
 #include <stdio.h>
 int main(void) {
 printf("Hello World!\n");
 printf("software engineering is my major\n");
+printf ("Integrated Science is my major\n");
 return 0;
 }
