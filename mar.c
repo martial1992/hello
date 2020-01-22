b710880c009b4e007d9c9ac0c99f417cf1d62775
martial1992 <gsandeudzondeu2@huskers.unl.edu>
2020-01-22 15:31:59 -0600

    Add math and update others

--- mar.c
+++ mar.c
@@ -1,5 +1,5 @@
 //This is my new code
-
+//Mike is a cool CL
 int main(void){
   printf("My new code");
   return 0;
---
+++ math.c
@@ -0,0 +1,7 @@
+//math code
+int main(){
+  int x = 8;
+  int y = 45;
+  printf("%d + %d is : %d\n", x , y, x + y);
+  return 0;
+}
